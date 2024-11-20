b5f84f314a49a7efc50172f826519c6212950da9
Teodor <raduteodorionut@gmail.com>
2024-11-21 00:14:53 +0200

    styled the right part

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const titlu = document.getElementById('titlu');
+const titlu = document.getElementById('titluProces');
 const titluNou = document.getElementById('titluNou');
 const save = document.getElementById('save');
 
 save.addEventListener('click', () => {
   const nume = titluNou.value;
   titlu.innerHTML = nume;
+  titlu.style.color = 'black';
 });
